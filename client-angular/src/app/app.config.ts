0898827e29feed1aab64a9b5bd871e68c0915f80
Christopher Buecheler <chris@embeddable.com>
2025-08-26 15:50:52 +0200

    Angular application works with token fetching

--- client-angular/src/app/app.config.ts
+++ client-angular/src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };

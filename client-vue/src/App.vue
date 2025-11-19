6e6cf3c035a53bfd9b50fbeb65c289ea782b3c7b
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2025-11-19 16:54:10 +0100

    add client-vue example

---
+++ client-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const token = ref('');
+const embedID = '<id of the embeddable you want to embed>';
+
+async function fetchToken() {
+  const response = await fetch('/token?embeddableId=' + embedID);
+  const data: { token: string } = await response.json();
+  token.value = data.token;
+}
+
+fetchToken();
+</script>
+
+<template>
+  <em-beddable v-if="token" :token="token"></em-beddable>
+</template>
+
+<style scoped></style>
---
+++ client-vue/vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes('-'),
+        },
+      },
+    }),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/token': {
+        target: 'http://localhost:3001',
+      },
+    },
+  },
+});
